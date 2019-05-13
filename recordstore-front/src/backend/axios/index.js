a476bee4dc28d5ab528b64c380a1dbde9cd23751
Yachnytskyi <yachnytskyi1992@gmail.com>
2019-05-13 08:43:14 +0300

    Add Vue.js and Setup Vue.js (recordstore-front), fix .gitignore and config/initializers/cors.rb

---
+++ recordstore-front/src/backend/axios/index.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+const API_URL = 'http://localhost:3000'
+
+const securedAxiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+const plainAxiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+securedAxiosInstance.interceptors.request.use(config => {
+  const method = config.method.toUpperCase()
+  if (method !== 'OPTIONS' && method !== 'GET') {
+    config.headers = {
+      ...config.headers,
+      'X-CSRF-TOKEN': localStorage.csrf
+    }
+  }
+  return config
+})
+
+export { securedAxiosInstance, plainAxiosInstance }
