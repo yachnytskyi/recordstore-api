2221b85a4eda0093928b67f69e4aebd2ef8e5d72
Yachnytskyi <yachnytskyi1992@gmail.com>
2019-06-03 08:07:16 +0300

    Add src/components/records/Record.vue, fix /src/App.vue and src/copponents/artists/Artists.vue

--- recordstore-front/src/components/artists/Artists.vue
+++ recordstore-front/src/components/artists/Artists.vue
@@ -43,3 +43,58 @@
     </ul>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Artists',
+  data () {
+    return {
+      artists: [],
+      newArtist: [],
+      error: '',
+      editedArtist: ''
+    }
+  },
+  created () {
+    if (!localStorage.signedIn) {
+      this.$router.replace('/')
+    } else {
+      this.$http.secured.get('/api/v1/artists')
+        .then(response => { this.artists = response.data })
+        .catch(error => this.setError(error, 'Something went wrong'))
+    }
+  },
+  methods: {
+    setError (error, text) {
+      this.error = (error.response && error.response.data && error.response.data.error) || text
+    },
+    addArtist () {
+      const value = this.newArtist
+      if (!value) {
+        return
+      }
+      this.$http.secured.post('/api/v1/artists/', { artist: { name: this.newArtist.name } })
+        .then(response => {
+          this.artists.push(response.data)
+          this.newArtist = ''
+        })
+        .catch(error => this.setError(error, 'Cannot create artist'))
+    },
+    removeArtist (artist) {
+      this.$http.secured.delete(`/api/v1/artists/${artist.id}`)
+        .then(response => {
+          this.artists.splice(this.artists.indexOf(artist), 1)
+        })
+        .catch(error => this.setError(error, 'Cannot delete artist'))
+    },
+    editArtist (artist) {
+      this.editedArtist = artist
+    },
+    updateArtist (artist) {
+      this.editedArtist = ''
+      this.$http.secured.patch(`/api/v1/artists/${artist.id}`, { artist: { title: artist.name } })
+        .catch(error => this.setError(error, 'Cannot update artist'))
+    }
+  }
+}
+</script>
